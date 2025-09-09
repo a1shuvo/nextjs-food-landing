dee2b3e7d6144fa97d36b565eb29dcf3c8b636e2
a1shuvo <a1shuvosaha@gmail.com>
2025-09-09 20:25:21 +0600

    Basic Homepage setup and font setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import Home from "@/components/Home/Home";
 
 const HomePage = () => {
   return (
-    <div className='text-red-500 text-6xl'>
-      Home Page
+    <div>
+      <Home />
     </div>
   );
 };
